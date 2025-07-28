ab418abc8fd6d2a42936b951f3dd05087891145c
Ivan Chernykh <time.nt.wait@gmail.com>
2025-07-28 23:05:01 +0300

    Добавлен main.py - общий пейплайн и добавлены заивсимисоти requirements.txt

---
+++ src/main.py
@@ -0,0 +1,47 @@
+# main.py
+"""
+Основной скрипт для запуска проекта.
+Здесь собирается весь процесс: подготовка данных, создание модели, обучение, оценка.
+Для запуска: python main.py --data_path path/to/processed.parquet --format parquet --limit 1000
+"""
+import torch
+import os
+
+from create_dataset import load_dataset, preprocess_text, create_datasets, generate_noisy_datasets
+from model_builder import create_peft_model, create_sentence_transformer
+from train import train_model
+from evaluate import evaluate_model
+
+import gc
+
+
+def main(data_path, limit=None):
+    print("Начинаем fine-tune TSDAE + QLoRA")
+
+    # Подготовка данных
+    df = load_dataset(data_path)
+    df = preprocess_text(df)
+    texts = df['text'].tolist()
+    train_texts, val_texts = create_datasets(texts, limit=limit)
+    train_dataset, val_dataset = generate_noisy_datasets(train_texts, val_texts)
+
+    # Создание модели
+    peft_model = create_peft_model()
+    sentence_model = create_sentence_transformer(peft_model)
+
+    # Обучение
+    train_model(sentence_model, train_dataset, val_dataset)
+
+    # Оценка
+    evaluate_model(sentence_model, val_texts)
+
+    # Освобождение памяти
+    torch.cuda.empty_cache()
+    gc.collect()
+
+    print(" Процесс завершен успешно!")
+
+if __name__ == "__main__":
+    # Путь к данным (важно чтобы в ДФ была колонка 'text')
+    DEFAULT_DATA_PATH = '../datasets/processed_texts.parquet'
+    main(data_path=DEFAULT_DATA_PATH, limit=200)
